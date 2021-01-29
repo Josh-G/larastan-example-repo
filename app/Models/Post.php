5d90cba70940e01a570b54d3e360e46387d7de7f
Josh Grant <josh@synergitech.co.uk>
2021-01-29 16:53:52 +0000

    demonstrate this also affects some third-party libs

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,25 @@ namespace App\Models;
 
 use App\User;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
 
 class Post extends Model
 {
+    use Sluggable, SluggableScopeHelpers;
+
+    /**
+     * @return array{slug: array{source: string}}
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
     public string $property_belonging_to_post = '';
 
     public function testMethodReturningCollectionAndModelInUnion(): void
@@ -31,5 +47,19 @@ class Post extends Model
         $collectionOrModel = User::collectionStatic();
 
         \PHPStan\dumpType($collectionOrModel);
+    }
+
+    public function testThirdPartyLibraryReturningCollectionAndModelInUnion(): void
+    {
+        // as per https://github.com/cviebrock/eloquent-sluggable/blob/a86500442ae8b1e2d965acb0340a1b867bf6c0f5/src/SluggableScopeHelpers.php#L78
+        // the return type of this trait method is:
+        // \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection
+        $post = Post::findBySlugOrFail('slug');
+
+        \PHPStan\dumpType($post);
+
+        if ($post instanceof Model) {
+            echo $post->property_belonging_to_post;
+        }
     }
 }
